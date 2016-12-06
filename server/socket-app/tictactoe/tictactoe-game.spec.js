8869bea3353f344008ceeafebc7b7895579dece1
Guðlaugur S. Egilsson <gulli@kolibri.is>
2016-12-06 14:43:12 +0000

    Rollback demo changes.

--- server/socket-app/tictactoe/tictactoe-game.spec.js
+++ server/socket-app/tictactoe/tictactoe-game.spec.js
@@ -7,30 +7,49 @@ var tictactoe = require('./tictactoe-handler')(inject({
     TictactoeState
 }));
 
-describe('create game command', function () {
+var createEvent = {
+    type: "GameCreated",
+    user: {
+        userName: "TheGuy"
+    },
+    name: "TheFirstGame",
+    timeStamp: "2014-12-02T11:29:29"
+};
+
+var joinEvent = {
+    type: "GameJoined",
+    user: {
+        userName: "Gummi"
+    },
+    name: "TheFirstGame",
+    timeStamp: "2014-12-02T11:29:29"
+};
+
+
+describe('create game command', function() {
 
 
     var given, when, then;
 
-    beforeEach(function () {
-        given = undefined;
-        when = undefined;
-        then = undefined;
+    beforeEach(function(){
+        given=undefined;
+        when=undefined;
+        then=undefined;
     });
 
     afterEach(function () {
-        tictactoe(given).executeCommand(when, function (actualEvents) {
+        tictactoe(given).executeCommand(when, function(actualEvents){
             should(JSON.stringify(actualEvents)).be.exactly(JSON.stringify(then));
         });
     });
 
 
-    it('should emit game created event', function () {
+    it('should emit game created event', function(){
 
         given = [];
         when =
         {
-            id: "123987",
+            id:"123987",
             type: "CreateGame",
             user: {
                 userName: "TheGuy"
@@ -46,7 +65,7 @@ describe('create game command', function () {
                 },
                 name: "TheFirstGame",
                 timeStamp: "2014-12-02T11:29:29",
-                side: 'X'
+                side:'X'
             }
         ];
 
@@ -100,7 +119,7 @@ describe('join game command', function () {
                 },
                 name: "TheFirstGame",
                 timeStamp: "2014-12-02T11:29:29",
-                side: 'O'
+                side:'O'
             }
         ];
 
@@ -108,46 +127,7 @@ describe('join game command', function () {
 
     it('should emit FullGameJoinAttempted event when game full..implement this', function () {
 
-
-        given = [
-            {
-                type: "GameCreated",
-                user: {
-                    userName: "TheGuy"
-                },
-                name: "TheFirstGame",
-                timeStamp: "2014-12-02T11:29:29"
-            },
-            {
-                type: "GameJoined",
-                user: {
-                    userName: "Gummi"
-                },
-                name: "TheFirstGame",
-                timeStamp: "2014-12-02T11:29:29",
-                side: 'O'
-            }
-        ];
-        when =
-        {
-            type: "JoinGame",
-            user: {
-                userName: "Gulli"
-            },
-            name: "TheFirstGame",
-            timeStamp: "2014-12-02T11:30:29"
-        };
-        then = [
-            {
-                type: "FullGameJoinAttempted",
-                user: {
-                    userName: "Gulli"
-                },
-                name: "TheFirstGame",
-                timeStamp: "2014-12-02T11:30:29"
-            }
-        ];
-
+        expect(true).toBe(false);
     });
 });
 
--- server/socket-app/tictactoe/tictactoe-state.js
+++ server/socket-app/tictactoe/tictactoe-state.js
@@ -4,27 +4,16 @@ module.exports = function (injected) {
 
     return function (history) {
 
-        var gamefull=false;
-
         function processEvent(event) {
-            if(event.type==="GameJoined"){
-                gamefull=true;
-            }
         }
 
         function processEvents(history) {
             _.each(history, processEvent);
         }
 
-        function gameFull(){
-            return gamefull;
-        }
-
         processEvents(history);
 
         return {
-
-            gameFull:gameFull,
             processEvents: processEvents
         }
     };

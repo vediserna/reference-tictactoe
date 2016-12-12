7cc400410798f0ff763e46ff0d3e2b22faac1c13
vediserna <vedise13@ru.is>
2016-12-12 13:35:44 +0000

    working on testscript

--- tests.sh
+++ tests.sh
@@ -4,17 +4,20 @@ echo "     _________________"
 echo "*~*~|  Running tests  |~*~*"
 echo "    |_________________|"
 
-echo "Running server tests..."
-npm run unittest
-rc=$?
-if [[ $rc != 0 ]] ; then
-    echo "npm run test failed with exit code " $rc
-    exit $rc
-fi
+#TODO
+    #COMMENT THESE LINES BACK IN
+
+#echo "Running server tests..."
+#npm run unittest
+#rc=$?
+#if [[ $rc != 0 ]] ; then
+#    echo "npm run test failed with exit code " $rc
+#    exit $rc
+#fi
 
 cd client
 echo "Running client tests..."
-npm run unittest
+npm run test
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "npm run test failed with exit code " $rc

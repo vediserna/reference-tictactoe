8cb3027db3dfc4d68567ecf983bc1751d355abdb
vediserna <vedise13@ru.is>
2016-12-14 14:20:50 +0000

    fixed a typo

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo "Copying docker-compose.yml to aws"
 scp -i ~/my-ec2-key-pair-vediserna.pem ~/workspace/hgop_tictactoe/docker-compose.yml ec2-user@ec2-54-191-32-51.us-west-2.compute.amazonaws.com:~/.
 
 echo "Running amazonscript.sh on aws"
-ssh -i ~/my-ec2-key-pair-vediserna.pem ec2-user@ec2-54-191-32-51.us-west-2.compute.amazonaws.com < ~/workspace/hgpo_tictactoe/provisioning/amazonscript.sh
+ssh -i ~/my-ec2-key-pair-vediserna.pem ec2-user@ec2-54-191-32-51.us-west-2.compute.amazonaws.com < ~/workspace/hgop_tictactoe/provisioning/amazonscript.sh
 #cd ..
 #echo "Copying .env folder to aws so GIT_COMMIT variable exists there with correct value"
 #scp -i ~/Documents/keys/my-ec2-key-pair-vediserna.pem ./.env ec2-user@ec2-54-191-32-51.us-west-2.compute.amazonaws.com:~/.

29bc1fcc9cf01b9c4680d3c82c456c1771d2d823
vediserna <vedise13@ru.is>
2016-12-14 00:13:07 +0000

    working on scripts, not working for GIT_COMMIT jenkins->aws

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,17 @@ echo "     ___________ "
 echo "*~*~| Deploying |~*~*"
 echo "    |___________|"
 
+echo Cleaning...
+rm -rf ./dist
+
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
 echo "Saving GIT_COMMIT value to githash.txt"
 cat > ./build/githash.txt <<_EOF_
 $GIT_COMMIT
--- tests.sh
+++ tests.sh
@@ -4,16 +4,13 @@ echo "     _________________"
 echo "*~*~|  Running tests  |~*~*"
 echo "    |_________________|"
 
-#TODO
-    #COMMENT THESE LINES BACK IN
-
-#echo "Running server tests..."
-#npm run unittest
-#rc=$?
-#if [[ $rc != 0 ]] ; then
-#    echo "npm run test failed with exit code " $rc
-#    exit $rc
-#fi
+echo "Running server tests..."
+npm run unittest
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "npm run test failed with exit code " $rc
+    exit $rc
+fi
 
 cd client
 echo "Running client tests..."

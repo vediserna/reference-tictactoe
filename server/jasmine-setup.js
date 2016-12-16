49ae9aeddf36a4d2b2bb59c111752d8e5808f03f
vediserna <vedise13@ru.is>
2016-12-16 14:10:58 +0000

    got jasmine to save xml files

--- server/jasmine-setup.js
+++ server/jasmine-setup.js
@@ -2,6 +2,8 @@ require('./globals');
 
 
 var JasmineConsoleReporter = require('jasmine-console-reporter');
+var jasmineReporters = require('jasmine-reporters');
+
 var consoleReporter = new JasmineConsoleReporter({
     colors: 1,           // (0|false)|(1|true)|2
     cleanStack: 1,       // (0|false)|(1|true)|2|3
@@ -9,8 +11,10 @@ var consoleReporter = new JasmineConsoleReporter({
     listStyle: 'indent', // "flat"|"indent"
     activity: false
 });
+jasmine.getEnv().addReporter(consoleReporter);
+
 var junitReporter = new jasmineReporters.JUnitXmlReporter({
-    savePath: '../junitxmlfile.xml',
+    savePath: 'tests',
     consolidateAll: false
 });
-jasmine.getEnv().addReporter(consoleReporter);
+jasmine.getEnv().addReporter(junitReporter);

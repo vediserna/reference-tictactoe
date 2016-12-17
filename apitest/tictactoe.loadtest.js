3425cf853523041f5273d24db81fd845cd295676
vediserna <vedise13@ru.is>
2016-12-17 23:18:21 +0000

    fixed loadtests

--- apitest/tictactoe.loadtest.js
+++ apitest/tictactoe.loadtest.js
@@ -26,36 +26,77 @@ describe('Tictactoe Loadtest', function () {
         });
     });
 
-    const count = 200;
-    const timelimit = 20000;
+    const count = 100;
+    const timelimit = 10000;
 
-    it('should be able to play ' + count + ' games to end in under ' + timelimit + 'ms', function (done) {
+    fit('should be able to play ' + count + ' games to end in under ' + timelimit + 'ms', function (done) {
         var startMillis = new Date().getTime();
 
         var user;
         var users=[];
-        for(var i=0; i<count; i++){
+        for(var i=0; i<count-2; i++){
             user = userAPI("User#" + i);
             users.push(user);
         }
 
-        user = userAPI("Final user");
-        user.expectGameCreated()
-            .createGame()
-            .then(function(){
-                user.disconnect();
-                _.each(users, function(usr){
-                    usr.disconnect();
-                });
-
-                var endMillis = new Date().getTime();
-                var duration = endMillis - startMillis;
-                if(duration > timelimit){
-                    done.fail(duration + " exceeds limit " + timelimit);
-                } else {
-                    done();
-
-                }
+         for(var i = 0; i < count-2; i++) {
+            var playerOne = users[i];
+            i++;
+            var playerTwo = users[i];
+            
+             playerOne.expectGameCreated().createGame().then(()=> {
+                playerTwo.expectGameJoined().joinGame(playerOne.getGame().gameId).then(function () {
+                    playerOne.expectMoveMade().placeMove(0).then(()=> {
+                        playerOne.expectMoveMade();
+                        playerTwo.expectMoveMade().placeMove(3).then(()=> {
+                            playerTwo.expectMoveMade(); // By other user
+                            playerOne.expectMoveMade().placeMove(4).then(()=> {
+                                playerOne.expectMoveMade(); // By other user
+                                playerTwo.expectMoveMade().placeMove(2).then(()=> {
+                                    playerTwo.expectMoveMade(); // By other user
+                                    playerOne.expectMoveMade().placeMove(8)
+                                        .expectGameWon().then(done); // Winning move
+                                })
+                            })
+                        });
+                    })
+                })
             });
+        }
+
+        playerOne = userAPI("Final playerOne");
+        playerTwo = userAPI("Final playerTwo");
+        playerOne.expectGameCreated().createGame().then(()=> {
+                playerTwo.expectGameJoined().joinGame(playerOne.getGame().gameId).then(function () {
+                    playerOne.expectMoveMade().placeMove(0).then(()=> {
+                        playerOne.expectMoveMade();
+                        playerTwo.expectMoveMade().placeMove(3).then(()=> {
+                            playerTwo.expectMoveMade(); // By other user
+                            playerOne.expectMoveMade().placeMove(4).then(()=> {
+                                playerOne.expectMoveMade(); // By other user
+                                playerTwo.expectMoveMade().placeMove(2).then(()=> {
+                                    playerTwo.expectMoveMade(); // By other user
+                                    playerOne.expectMoveMade().placeMove(8)
+                                        .expectGameWon().then(done); // Winning move
+                                })
+                            })
+                        });
+                    })
+                })
+            }).then(function(){
+                    playerOne.disconnect();
+                    playerTwo.disconnect();
+                    _.each(users, function(usr){
+                        usr.disconnect();
+                    });
+
+                    var endMillis = new Date().getTime();
+                    var duration = endMillis - startMillis;
+                    if(duration > timelimit){
+                        done.fail(duration + " exceeds limit " + timelimit);
+                    } else {
+                        done();
+                    }
+           });
     });
 });
--- client/src/routing-context.js
+++ client/src/routing-context.js
@@ -13,7 +13,7 @@ function routingContext(injected){
     const environment = injected('env');
     var socketURI;
     if(environment==='development' || environment==='test'){
-        socketURI='http://54.191.32.51'
+        socketURI='http://localhost:8080'
     } else {
         socketURI='/'
     }
